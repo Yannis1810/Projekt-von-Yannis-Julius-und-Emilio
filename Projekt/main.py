05c45e4c87804657e4fff57f6a2dbb4393d3c189
Yannis1810 <164043914+Yannis1810@users.noreply.github.com>
2024-07-01 15:02:13 +0200

    Letzter Commit Rückgängig
     wegen Dopplungen

--- Projekt/main.py
+++
@@ -1,6 +0,0 @@
-from website import create_app # create_app() wird von __init__.py importiert
-
-app = create_app()
-
-if __name__ == '__main__': # hier wird die App nur gestartet, wenn sie __main__ heißt, was autom. passiert durch den Import
-    app.run(debug=True) # damit man nicht immer neu starten muss, sondern die Änderungen mit dem Speichern direkt geladen werden
